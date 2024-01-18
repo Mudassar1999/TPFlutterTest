d425d762986bd77af76a6147ff12a23f231c5fbc
Muhammad Mudassar Hameed <57958613+Mudassar1999@users.noreply.github.com>
2024-01-18 20:50:49 +0500

    Update categories.routes.js

--- src/routes/v1/categories/categories.routes.js
+++ src/routes/v1/categories/categories.routes.js
@@ -1,60 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
-const httpStatus = require("http-status");
+const data = require("../../../../data.json");
 
 router.route("/").get(async (req, res) => {
   try {
-    // const categories =[
-    //   {
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   },
-    //   {
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   },
-    //   {
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   },{
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   },{
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   },
-    //   {
-    //     name:"",
-    //     image:"",
-    //     quantity:,
-    //     status: 
-    //   }
-    // ]
-    // res.status(200).json(categories);
+    res.status(200).json(data);
   } catch (error) {
     throw error;
   }
 });
-
-router.route("/list").get(async (req, res) => {
-  try {
-    res.status(200).json({
-      status:"Success"
-    });
-  } catch (error) {
-    throw error;
-  }
-});
-
 module.exports = router;
